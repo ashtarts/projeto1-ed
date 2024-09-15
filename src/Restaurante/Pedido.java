bf66c996b51bb8b2854e11fe0bff775b01c1c0b7
sergiomelofdev <sergio.00000847551@unicap.br>
2024-09-14 22:21:10 -0300

    Restaurante
    implementação incompleta da parte do restaurante, foram adicionadas as classes mais importantes, porém ainda falta criar a parte gráfica.

---
+++ src/Restaurante/Pedido.java
@@ -0,0 +1,19 @@
+package Restaurante;
+
+public class Pedido {
+    String descricao;
+    int quantidade;
+    double total;
+
+    public Pedido(String descricao, int quantidade, double total) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.total = total;
+    }
+
+    public String toString() {
+        return descricao + " - Quantidade: " + quantidade + "-Total: " +total;
+    }
+
+    
+}
